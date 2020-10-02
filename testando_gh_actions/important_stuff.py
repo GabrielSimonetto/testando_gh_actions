a53b93ed8af8deedab8abe27c2d02f0890e02eda
Gabriel Simonetto <gabrielfranksimonetto@gmail.com>
2020-10-02 20:39:26 -0300

    Inicial commit

---
+++ testando_gh_actions/important_stuff.py
@@ -0,0 +1,8 @@
+def sum(a,b):
+    if a == 1 and b == 1:
+        return 2
+    elif a == 2 and b == 2:
+        return 4
+    else:
+        raise NotImplementedError
+    
---
+++ tests/test_sum.py
@@ -0,0 +1,14 @@
+from pytest import raises
+
+from testando_gh_actions.important_stuff import sum
+
+
+def test_sum_1_and_1():
+    assert sum(1,1) == 2
+
+def test_sum_2_and_2_correct():
+    assert sum(2,2) == 4
+
+def test_sum_9_and_9():
+    with raises(NotImplementedError):
+        sum(9,9)
