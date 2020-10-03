bf6ff6259baa7f155e54b2b66b2a14cda27c7e42
Gabriel Simonetto <gabrielfranksimonetto@gmail.com>
2020-10-03 15:56:16 -0300

    missing coverage

--- testando_gh_actions/important_stuff.py
+++ testando_gh_actions/important_stuff.py
@@ -3,6 +3,12 @@ def sum(a,b):
         return 2
     elif a == 2 and b == 2:
         return 4
+    elif a == 5 and b == 5:
+        return 10
     else:
         raise NotImplementedError
+
+def sub(a,b):
+    if a == 1 and b == 1:
+        return 0
     
